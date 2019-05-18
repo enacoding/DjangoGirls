6aab5fa606d88c21acceb7988aad45adf5b4028b
Lorena <lorena@usuario.com>
2019-05-18 16:24:26 +0200

    Orden web

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
 
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('published_date', 'created_date')
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('author', 'published_date')
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
-class Post (models.Model):
+class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField (default=timezone.now)
-    published_date = models.DateTimeField (blank=True, null=True)
+    published_date = models.DateTimeField (default=timezone.now, blank=False, null=False)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -14,6 +14,4 @@ class Post (models.Model):
 
     def __str__(self):
         return self.title
-
-
 
